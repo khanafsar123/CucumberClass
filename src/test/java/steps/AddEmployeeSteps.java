d24d1c55a28f58e5da1b78ffc23dc460ec5718e7
Khan Afsar <camrose2022@hotmail.com>
2022-12-07 16:59:41 -0800

    12/7/2022

---
+++ src/test/java/steps/AddEmployeeSteps.java
@@ -0,0 +1,33 @@
+package steps;
+
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class AddEmployee {
+    @When("user clicks on PIM option")
+    public void user_clicks_on_pim_option() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @When("user clicks on Add Employee button")
+    public void user_clicks_on_add_employee_button() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @When("user enter firstname and lastname")
+    public void user_enter_firstname_and_lastname() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @When("user clicks on save button")
+    public void user_clicks_on_save_button() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @Then("employee added successfully")
+    public void employee_added_successfully() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+}
